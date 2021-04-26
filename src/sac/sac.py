Server changes - fixed scaling action issues and other various bugs that came up during testing, added additional logging

src/sac/sac.py
@@ -11,7 +11,8 @@ class SACAgent:
     def __init__(self,
                  action_range, action_dim, gamma, tau, v_lr, q_lr, pi_lr, buffer_maxlen=int(1e6),
                  image_size=(256,256,3), kernel_size=(3,3), conv_channels=4,
-                 checkpoint=None):
+                 checkpoint=None,
+                 logFile='logs/losses.txt'):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.action_range = action_range
@@ -22,6 +23,9 @@ class SACAgent:
         self.tau = tau
         self.update_step = 0
         self.delay_step = 2
+
+        # Logging
+        self.logFile = open(logFile, 'w')
 
         if checkpoint == None: checkpoint = {}
         saved_fe = checkpoint.get('fe', None)
@@ -66,32 +70,6 @@ class SACAgent:
 
         self.replay_buffer = BasicBuffer(buffer_maxlen)
 
-    def get_action(self, state):
-        input = torch.from_numpy(state).float().permute(2, 0, 1).unsqueeze(0).to(self.device)
-        features = self.fe(input)
-        features = features.view(-1, self.in_dim)
-
-        mean, log_std = self.policy_net.forward(features)
-        std = log_std.exp()
-
-        normal = Normal(mean, std)
-        z = normal.sample()
-        action = torch.tanh(z)
-        action = action.cpu().detach().squeeze(0).numpy()
-
-        return self.rescale_action(action)
-
-    def rescale_action(self, action):
-        print('action: ', action)
-        scaled_action = []
-        for idx, a in enumerate(action):
-            action_range = self.action_range[idx]
-            a = a * (action_range[1] - action_range[0]) / 2.0 + (action_range[1] + action_range[0]) / 2.0
-            scaled_action.append(a)
-        scaled_action[0] += 50
-        print('scaled action: ', scaled_action)
-        return scaled_action
-
     def update(self, batch_size):
         if len(self.replay_buffer) <= batch_size:
             print('Replay buffer not large enough to sample, returning models...')
@@ -122,6 +100,7 @@ class SACAgent:
         next_v_target = torch.min(next_q1, next_q2) - next_log_pi
         curr_v = self.v_net.forward(features)
         v_loss = F.mse_loss(curr_v, next_v_target.detach())
+        self.logFile.write(' v_loss: %s\n' % v_loss.item())
 
         # Q loss
         curr_q1 = self.q_net1.forward(features, actions)
@@ -129,6 +108,8 @@ class SACAgent:
         expected_q = rewards + (1 - dones) * self.gamma * next_v
         q1_loss = F.mse_loss(curr_q1, expected_q.detach())
         q2_loss = F.mse_loss(curr_q2, expected_q.detach())
+        self.logFile.write('q1_loss: %s\n' % q1_loss.item())
+        self.logFile.write('q2_loss: %s\n' % q2_loss.item())
 
         # update v_net and q_nets
         self.v_optimizer.zero_grad()
@@ -150,6 +131,7 @@ class SACAgent:
                 self.q_net2.forward(features, new_actions)
             )
             pi_loss = (log_pi - min_q).mean()
+            self.logFile.write('pi_loss: %s\n\n' % pi_loss.item())
 
             self.pi_optimizer.zero_grad()
             pi_loss.backward(retain_graph=True)
@@ -157,6 +139,8 @@ class SACAgent:
 
             for target_param, param in zip(self.target_v_net.parameters(), self.v_net.parameters()):
                 target_param.data.copy_(self.tau * param + (1 - self.tau) * target_param)
+        else:
+            self.logFile.write('\n\n')
 
         self.update_step += 1
         return self.fe.state_dict(), self.pi_net.state_dict(), True